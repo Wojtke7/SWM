d87a851ee99f1ee308fc11a2f0130d70ad750299
wmarcela <wmarcela@student.agh.edu.pl>
2023-05-02 16:34:58 +0200

    Task upgraded, faster function added

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import numpy
 random_range = random.randint(10, 10000)
 
 # Fill both arrays
-A = numpy.random.randint(0, 10, random_range)
-B = numpy.random.randint(0, 10, random_range)
+A = numpy.random.randint(0, 20, random_range)
+B = numpy.random.randint(0, 20, random_range)
 
 print(f"Amount of elements: {random_range}")
 print(A)
@@ -16,6 +16,7 @@ print(B)
 
 
 # Function that checks if number is prime
+# Time complexity sqrt(n)
 def is_prime(number):
     if number < 2:
         return False
@@ -26,8 +27,22 @@ def is_prime(number):
     return True
 
 
-# Main function
+# Main function time complexity O(n * sqrt(n))
 def func(seq_A, seq_B):
+    set_A = set(seq_A)
+    set_B = set(seq_B)
+
+    counter_B = {num_b: numpy.count_nonzero(seq_B == num_b) for num_b in set_B}
+
+    # print(counter_B)
+
+    C = [num_a for num_a in set_A if not is_prime(counter_B.get(num_a, 0))]
+
+    return C
+
+
+# Simpler second function time complexity O(n^2 * sqrt(n))
+def second_func(seq_A, seq_B):
     C = []
 
     for num_a in seq_A:
@@ -40,16 +55,16 @@ def func(seq_A, seq_B):
         if not is_prime(counter):
             C.append(num_a)
 
-    return C
-
 
 # Execution
 print("Starting function execution...")
 start_time = time.time()
+
 c = func(A, B)
+
 end_time = time.time()
-total = end_time - start_time
 print("Function execution finished.")
 
+duration = end_time - start_time
 print(c)
-print(f"Total time: {total}")
+print(f"Total time: {duration}")
