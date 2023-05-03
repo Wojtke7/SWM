605182fd0b4276f839b7aecf6c06d23eeb937583
wmarcela <wmarcela@student.agh.edu.pl>
2023-05-03 18:29:59 +0200

    Changes in main function

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import math
 import random
 import time
-import numpy
+import numpy as np
 
-# Choose random amount of elements in both arrays
+# Lists from example
+example_A = np.array([2, 3, 9, 2, 5, 1, 3, 7, 10])
+example_B = np.array([2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10])
+
+# Choose random amount of elements in both lists
 random_range = random.randint(10, 10000)
 
-# Fill both arrays
-A = numpy.random.randint(0, 20, random_range)
-B = numpy.random.randint(0, 20, random_range)
+# Fill both lists
+A = np.random.randint(0, 20, random_range)
+B = np.random.randint(0, 20, random_range)
 
 print(f"Amount of elements: {random_range}")
-print(A)
-print(B)
+print(f"List A: {A}")
+print(f"List B: {B}")
 
 
-# Function that checks if number is prime
-# Time complexity sqrt(n)
+# Function that checks if number is prime, time complexity sqrt(n)
 def is_prime(number):
     if number < 2:
         return False
@@ -27,16 +30,15 @@ def is_prime(number):
     return True
 
 
-# Main function time complexity O(n * sqrt(n))
+# Main function, time complexity O(n * sqrt(n))
 def func(seq_A, seq_B):
-    set_A = set(seq_A)
     set_B = set(seq_B)
 
-    counter_B = {num_b: numpy.count_nonzero(seq_B == num_b) for num_b in set_B}
+    counter_B = {num_b: np.count_nonzero(seq_B == num_b) for num_b in set_B}
 
     # print(counter_B)
 
-    C = [num_a for num_a in set_A if not is_prime(counter_B.get(num_a, 0))]
+    C = [num_a for num_a in seq_A if not is_prime(counter_B.get(num_a, 0))]
 
     return C
 
@@ -45,11 +47,15 @@ def func(seq_A, seq_B):
 print("Starting function execution...")
 start_time = time.time()
 
-c = func(A, B)
+c = func(B, A)
 
 end_time = time.time()
-print("Function execution finished.")
+print("Function execution finished. \n")
 
 duration = end_time - start_time
 print(c)
-print(f"Total time: {duration}")
+print(f"Total time: {duration} \n")
+
+# Execution from example
+d = func(example_A, example_B)
+print(f"Example from task: {d}")
